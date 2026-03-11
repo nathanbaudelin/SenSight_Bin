d895dcfe1e5a1ac756d2e15014bac2aff5032376
Nathan Baudelin <nathan.baudelin@epitech.eu>
2026-03-11 11:12:05 +0100

    fix(docker): docker with new project name

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,7 +9,11 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'docker' ? '../.env' : '.env',
     }),
-    MongooseModule.forRoot(process.env.MONGO_URL + '/smartbin'),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL
+        ? process.env.MONGO_URL
+        : 'mongodb://mongodb:27017/sensight_bin',
+    ),
   ],
   controllers: [AppController],
 })
